6209d00c969c79b1be385b51cf66b681abcde475
Julian Groß <firedefender1@googlemail.com>
2020-12-23 07:14:01 +0100

    Hack: remove nvcore Debug.cpp

--- cmake/ports/nvtt/portfile.cmake
+++ cmake/ports/nvtt/portfile.cmake
@@ -15,6 +15,13 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL arm64)
+    vcpkg_apply_patches(
+        SOURCE_PATH ${SOURCE_PATH}
+        PATCHES "${CMAKE_CURRENT_LIST_DIR}/0001-Hack-to-build-on-linux-aarch64.patch"
+    )
+endif()
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
