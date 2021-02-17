cc989d480736b1688d22a0fde0f028db76f9bc22
Julian Groß <firedefender1@googlemail.com>
2021-02-17 04:58:52 +0100

    Patch nvtt to build on aarch64

--- cmake/ports/nvtt/portfile.cmake
+++ cmake/ports/nvtt/portfile.cmake
@@ -18,7 +18,7 @@ vcpkg_from_github(
 if(VCPKG_TARGET_ARCHITECTURE STREQUAL arm64)
     vcpkg_apply_patches(
         SOURCE_PATH ${SOURCE_PATH}
-        PATCHES "${CMAKE_CURRENT_LIST_DIR}/0001-Hack-to-build-on-linux-aarch64.patch" "${CMAKE_CURRENT_LIST_DIR}/0002-Hack-remove-include-sysctl.h-to-build-on-linux-a.patch"
+        PATCHES "${CMAKE_CURRENT_LIST_DIR}/0001-Build-on-aarch64.patch" "${CMAKE_CURRENT_LIST_DIR}/0002-Hack-remove-include-sysctl.h-to-build-on-linux-a.patch"
     )
 endif()
 
