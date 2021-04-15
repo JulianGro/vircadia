2b8d6fc10c02f20cf27ba18deb3f423cb6ae0db3
Julian Groß <firedefender1@googlemail.com>
2021-04-15 16:27:43 +0200

    portfile.cmake aktualisiert

--- cmake/ports/assimp/portfile.cmake
+++ cmake/ports/assimp/portfile.cmake
@@ -21,6 +21,5 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake)
-vcpkg_fixup_cmake_targets(CONFIG_PATH debug/lib/cmake)
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
