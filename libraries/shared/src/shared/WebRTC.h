fdf5d7cbeb0d00c81eea4a8ecbd538419f4e36d1
Julian Groß <firedefender1@googlemail.com>
2021-09-15 21:30:16 +0200

    WebRTC.h aktualisiert

--- libraries/shared/src/shared/WebRTC.h
+++ libraries/shared/src/shared/WebRTC.h
@@ -26,7 +26,6 @@
 #elif defined(Q_OS_LINUX) && defined(Q_PROCESSOR_X86_64)
 #  define WEBRTC_ENABLED 1
 #  define WEBRTC_POSIX 1
-#elif
 #elif defined(Q_OS_LINUX) && defined(Q_PROCESSOR_ARM)
 // WebRTC is basically impossible to build on aarch64 Linux.
 // I am looking at https://gitlab.freedesktop.org/pulseaudio/webrtc-audio-processing for an alternative.
